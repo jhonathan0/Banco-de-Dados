289eb48cb6333be735e86baf673f22d26dc28b11
jhonathan0 <147885506+jhonathan0@users.noreply.github.com>
2024-10-15 12:06:31 -0300

    .

---
+++ script.py
@@ -0,0 +1,16 @@
+print ("Faça login para conhecer a felicidade !\n")
+
+NOME= input ("Digite seu Nome \n")
+
+IDADE = input ("Digite sua Idade \n")
+
+print (NOME + IDADE)
+
+CPF = input ("Digite seu CPF \n")
+
+EMAIL = input ("Digite seu EMAIL \n")
+
+NUMERO= input ("Digite seu NÚMERO \n")
+
+
+print ("Seu login está pronto, seja feliz..\n")
